b5fa15c0c2253b0a3c366d686652b961ea6a3aec
mohamudyare <mohamudyare067@gmail.com>
2026-04-27 20:44:36 +0300

    Submit Exercise 1

---
+++ Exercise 1/main.js
@@ -0,0 +1,19 @@
+
+let x = 7;
+let y = 3;
+
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+
+console.log(x/y);
+console.log(x%y);
+
+// increament
+console.log(++x);
+
+// Decreament
+console.log(--y);
+
+
