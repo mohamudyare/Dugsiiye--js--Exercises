00230747c3fec3a302b9a9823eead4834641bb9b
mohamudyare <mohamudyare067@gmail.com>
2026-04-29 17:52:43 +0300

    Submit Exercise 2

---
+++ Exercise 2/compa.js
@@ -0,0 +1,30 @@
+
+let a = 15;
+let b = "15";
+
+
+// Equal to
+console.log(a == b);
+// Not Eqeal to
+console.log(a != b);
+
+
+
+// Strictly Eqeal to
+console.log(a === b);
+// Srictly Not Eqeal to
+console.log(a !== b)
+
+
+
+// Greater Than
+console.log(a > b);
+// Less than
+console.log(a < b);
+
+
+
+// Greater than or Eqeal to
+console.log(a >= b);
+// Less than or Eqeal to
+console.log(a <= b);
