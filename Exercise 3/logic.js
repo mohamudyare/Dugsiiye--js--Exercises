3ccecd733dd85e5f4bceb8fb7613573e6d224495
mohamudyare <mohamudyare067@gmail.com>
2026-04-29 20:35:02 +0300

    submit Exercise 3

---
+++ Exercise 3/logic.js
@@ -0,0 +1,16 @@
+
+
+// Logic Operator
+let isSunny = true;
+let isWeekend = true;
+
+
+// AND &&
+console.log(isSunny && isWeekend);
+
+// OR ||
+console.log(isSunny || isWeekend);
+
+// NOT !
+console.log(!isSunny)
+
