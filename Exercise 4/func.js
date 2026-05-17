74febfcd99e8f47b43e2b80ac08700c64b9c4a54
mohamudyare <mohamudyare067@gmail.com>
2026-05-17 19:30:16 +0300

    add Exercise 4 js

---
+++ Exercise 4/func.js
@@ -0,0 +1,30 @@
+
+
+// 1) Function declaration 
+
+function  add(b,c) {
+     
+        console.log(b+c);
+}
+
+add(10,20);
+add(40,20);
+add(10,5);
+
+
+
+
+
+
+//2) Function EXpresion
+
+const addition = function(b,c) {
+
+        console.log(b+c);
+}
+
+
+addition(40,10);
+addition(30,8);
+addition(5,4);
+addition(80,10);
