f0da43ae72ad5df638027d6d7d49f7ca93e36e14
mohamudyare <mohamudyare067@gmail.com>
2026-05-19 21:02:55 +0300

    add Exercise 7 Arrays method

---
+++ Exercise 7/meth.js
@@ -0,0 +1,26 @@
+
+// Arrays Method
+
+
+// Before Array
+let colors = ["yellow" , "Blue" , "Green"]
+
+console.log(colors);
+
+
+
+
+
+// After push
+colors.push("Red");
+
+console.log(colors);
+
+
+
+
+
+// After shift
+colors.shift();
+
+console.log(colors);
