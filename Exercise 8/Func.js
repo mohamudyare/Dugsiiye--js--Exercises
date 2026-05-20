957d675f2c6440b40b28f6ea0aec007866750416
mohamudyare <mohamudyare067@gmail.com>
2026-05-20 19:05:31 +0300

    add Exercise 8 Functions

---
+++ Exercise 8/Func.js
@@ -0,0 +1,17 @@
+
+// Function Exercise
+
+
+
+let Car ={
+      make : "Toyota" , 
+      model : "corolla" ,
+      year : 2018   
+}
+
+
+console.log(Car.make);
+
+console.log(Car.model);
+
+console.log(Car.year);
