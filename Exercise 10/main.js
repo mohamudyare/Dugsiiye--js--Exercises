7ee1c9c43cc32724a1961762ce262323d0e4a6cf
mohamudyare <mohamudyare067@gmail.com>
2026-05-23 20:13:10 +0300

    add Exercise 10

---
+++ Exercise 10/main.js
@@ -0,0 +1,25 @@
+
+// Arrays of object
+
+
+let Library =[
+
+    {
+        title:"Atomic Habits" ,
+        author: "James Clear" ,
+        year: 2018
+    } ,
+
+    {
+        title: "Rich Dad Poor Dad",
+        author: "Robert Kiyosaki" ,
+        year: 1997
+    }
+]
+
+
+
+
+console.log(Library[0].title);
+
+console.log(Library[1].author);
