960a0776b0f8a76e3eff65dc57733aa5d487a391
mohamudyare <mohamudyare067@gmail.com>
2026-05-23 17:39:38 +0300

    add Exercise 9 method object

---
+++ Exercise 9/main.js
@@ -0,0 +1,24 @@
+
+
+// Method object
+
+
+let car = {
+
+     Make:"BMW" ,
+     model:"x5" ,
+     year:2022 ,
+
+     start : () => {
+            console.log("The car has started")
+     }
+
+}
+
+
+
+console.log(car.Make);
+console.log(car.model);
+console.log(car.year);
+
+console.log(car.start());
