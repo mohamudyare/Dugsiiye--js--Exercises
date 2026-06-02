971ebc099c913172b2a405bfaf461e6e067ec08c
mohamudyare <mohamudyare067@gmail.com>
2026-06-02 21:21:30 +0300

    add Exercise 17 else-if statement

---
+++ Exercise 17/else-if.js
@@ -0,0 +1,21 @@
+
+
+// else if statement
+
+let temperature = 20;
+
+if(temperature < 0){
+
+                    console.log("very cold");
+
+}else if(temperature < 15){
+
+                     console.log("Cold"); 
+
+}else if(temperature < 25){
+
+                       console.log("Warm");
+
+}else{
+                        console.log("Hot");
+}
