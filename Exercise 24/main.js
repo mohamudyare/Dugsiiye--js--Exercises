6e4c281e47e834d801ec602e6a73777719b1eac1
mohamudyare <mohamudyare067@gmail.com>
2026-06-10 21:07:23 +0300

    add Exercise 24

---
+++ Exercise 24/main.js
@@ -0,0 +1,17 @@
+
+
+// Default parameters
+
+
+function Area(width , height = 5) {
+
+            console.log(`AreaWidth: ${width}`)
+            console.log(`AreaHeight: ${height}`)
+}
+
+
+Area(5);
+
+
+// Otput  AreaWidth:  5
+// Output AreaHeight: 5
