f473bbd0d4382d7f127cd6610f486b8b2fdbacce
mohamudyare <mohamudyare067@gmail.com>
2026-06-20 21:08:56 +0300

    add Exercise 32

---
+++ Exercise 32/main.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+// queryselector id
+
+const header = document.querySelector('#head');
+
+console.log(header);
+
+
+
+
+
+
+
+
+// queryselectorAll class
+
+
+const para = document.querySelectorAll('.paragraph');
+
+console.log(para);
