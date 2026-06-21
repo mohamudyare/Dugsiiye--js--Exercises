7b6de7fe9728efc5bd8dce6ae478dc6f2dd8a51c
mohamudyare <mohamudyare067@gmail.com>
2026-06-21 18:51:15 +0300

    add Exercise 33

---
+++ Exercise 33/main.js
@@ -0,0 +1,36 @@
+
+const header = document.querySelector('#head1');
+
+console.log(header);
+
+
+
+
+const paragraph = document.querySelector('#para');
+
+console.log(paragraph);
+
+
+
+
+
+
+// textXContent
+
+function  changetext() {
+
+    head1.textContent = "WELCOME TO THE EXERCISE-33"
+}
+
+
+
+
+
+// innerHtml
+
+function  ChangeInner(){
+
+    para.innerHTML = `The Changing inner html <strong> consider to like my website </strong>`;
+}
+
+
